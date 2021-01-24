d8e217792d689f981071281b9a9b432090856382
Daniel <wilksjdaniel@gmail.com>
2021-01-24 23:06:01 +0000

    added Procfile

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log('Server started on port: ' + PORT);
  });
