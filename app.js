cad9dc2777f420d61748384e2a1a4d1958e3b255
Daniel <wilksjdaniel@gmail.com>
2021-01-24 04:22:09 +0000

    posts router passes tests

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const posts = require('./routes/posts')
-const port = process.env.PORT || 3000;
 const app = express();
 
 app.use('/posts', posts);
 
-app.listen(port, () => {
-    console.log('Server started on port: ' + port);
-});
+module.exports = app;
